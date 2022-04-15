10c21d02f597633de08e5f60be83e4aa1e86b92e
Karin Oldbring <karin.oldbring@gmail.com>
2022-04-15 10:24:00 +0000

    define play_game function and add print statements

--- run.py
+++ run.py
@@ -8,8 +8,22 @@ def get_valid_word(words):
     while "-" in word or " " in word: 
         word = random.choice(words)
     
-    return word
+    return word.upper()
 
 # Input username
 name = input("What's your name?")
 print(f"Good luck {name}!")
+
+turns = 6
+
+def play_game():
+    word = get_valid_word(words)
+    secret_word = "_" * len(word)
+    guessed = False
+    guessed_letters = []
+    turns = 6
+    print("Let's start!")
+    print(f"Turns left: {turns}")
+    print("Guess the word by choosing a letter: " + " ".join(secret_word) +"\n")
+
+play_game()
