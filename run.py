a4ee5eece0a950045d994de3708add67f12787de
Karin Oldbring <karin.oldbring@gmail.com>
2022-04-15 05:58:20 +0000

    Add input for username

--- run.py
+++ run.py
@@ -1,12 +1,15 @@
 import random
 from words import words
 
-""" 
-generate valid randomm word
-"""
+# generate valid random word
+
 def get_valid_word(words):
     word = random.choice(words)
     while "-" in word or " " in word: 
         word = random.choice(words)
     
     return word
+
+# Input username
+name = input("What's your name?")
+print(f"Good luck {name}!")
