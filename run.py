ee5a5954c9ae5481c78adcbc8ad40a239159346b
Karin Oldbring <karin.oldbring@gmail.com>
2022-04-15 05:46:28 +0000

    Create function to generate a valid random word

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
 import random
 from words import words
-print(words)
+
+""" 
+generate valid randomm word
+"""
+def get_valid_word(words):
+    word = random.choice(words)
+    while "-" in word or " " in word: 
+        word = random.choice(words)
+    
+    return word
